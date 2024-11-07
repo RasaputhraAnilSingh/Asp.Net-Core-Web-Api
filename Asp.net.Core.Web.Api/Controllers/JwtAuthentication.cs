b5194e62ca89229daea3b3fd663d8d6f8b4432bf
Anil Singh Rasaputhra <anil.rasaputhra@computechcorp.com>
2024-11-07 12:02:02 +0530

    Add project files.

---
+++ Asp.net.Core.Web.Api/Controllers/JwtAuthentication.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Asp.net.Core.Web.Api.Controllers
+{
+    [Route("Api/[controller]")]
+    [ApiController]
+    public class JwtAuthentication : ControllerBase
+    {
+        
+        [HttpGet]
+        public ActionResult<List<int>> Get()
+        {
+            List<int> values = new List<int>()
+            {
+                1,2,3,4,5,6
+            };
+            return Ok(values);
+        }
+    }
+}
